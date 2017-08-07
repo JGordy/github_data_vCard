340fd681bbcf150ddd2eba3ea5c0da658b9d6da1
Joseph Gordy <jgordy2424@gmail.com>
2017-08-07 15:48:22 -0400

    logged Github data on console

--- main.js
+++ main.js
@@ -73,12 +73,12 @@ vCardFunction();
 
 // Github api request
 
-// function reqListener () {
-//    let info = JSON.parse(this.responseText);
-// console.log(data);
-// }
-//
-// let req = new XMLHttpRequest();
-// req.open("GET", "https://api.github.com/users/jgordy");
-// req.addEventListener("load", reqListener);
-// req.send();
+function reqListener () {
+   let info = JSON.parse(this.responseText);
+console.log(data);
+}
+
+let req = new XMLHttpRequest();
+req.open("GET", "https://api.github.com/users/jgordy");
+req.addEventListener("load", reqListener);
+req.send();
