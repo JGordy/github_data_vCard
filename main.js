8726378cc8351402855c541ce8016876814b4b04
Joseph Gordy <jgordy2424@gmail.com>
2017-08-07 14:07:02 -0400

    pulled data from Github using api request

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
+//  select the parent elements
+let header = document.querySelector(".header");
+
+let list = document.querySelector(".list");
+
+let icon = document.querySelector(".icon");
+
+// github api request
+
 function reqListener () {
   let data = JSON.parse(this.responseText);
-  console.log(data);
+console.log(data);
+  header.innerHTML = `<h1>${data.name}</h1>`;
+
+  list.innerHTML = `<li>
+                      <span>Name: </span>${data.name}
+                    </li>
+                    <li>
+                      <span>Github URL: </span><a>${data.url}</a>
+                    </li>
+                    <li>
+                      <span>Email: </span><a>${data.email}</a>
+                    </li>
+                    <li>
+                      <span>Company: </span>${data.company}
+                    </li>
+                    <li>
+                      <span>Website: </span><a href="${data.blog}">${data.blog}</a>
+                    </li>`;
+
+  icon.innerHTML = `<img src=${data.avatar_url}>`;
+
+  console.log(data.name);
+  console.log(header.innerHTML);
 }
 
 let req = new XMLHttpRequest();
